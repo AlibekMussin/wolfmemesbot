06cbb7f31f9ad641f8705583dedeeb5a17c4849d
Алибек <svarog221989@gmail.com>
2023-04-12 11:39:15 +0600

    add pillow and alter fields

--- apps/request/models.py
+++ apps/request/models.py
@@ -3,7 +3,14 @@ from apps.utils.models import CommonFields
 
 
 class MemesRequest(CommonFields):
-    message = models.TextField(verbose_name='Текст заявки')
+    message = models.TextField(
+        verbose_name='Текст заявки',
+        null=True, blank=True)
+    image = models.ImageField(
+        upload_to='memes',
+        verbose_name='Картинка с мемом',
+        null=True, blank=True
+    )
     sender_id = models.IntegerField(verbose_name='Заявитель', null=True, blank=True)
 
     def __str__(self):
